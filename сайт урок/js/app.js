fa741d150084117b54eaabe4786de65d62915468
TagirAHAAH <151431473+TagirAHAAH@users.noreply.github.com>
2023-11-19 11:52:56 +0300

    Delete сайт урок directory

--- сайт урок/js/app.js
+++
@@ -1,7 +0,0 @@
-let imgBox = document.getElementById("imgBox")
-let qrText = document.getElementById("qrText")
-let qrImage = document.getElementById("qrImage")
-
-function genQR(){
-    qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value
-}
